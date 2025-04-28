e7c8e7574f630791cf778e5225319f8918d8baa8
Bella Makhova <rgvder@gmail.com>
2025-04-28 11:12:08 +0300

    feat: implement fs functions

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import { readdir } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+
+import { formatFsPath } from '../utils/fs/formatFsPath.js';
+import { FsError } from '../utils/fs/FsError.js';
+
+const folderPath = formatFsPath('files');
+
 const list = async () => {
-    // Write your code here 
+  if (!existsSync(folderPath)) {
+    throw new FsError();
+  }
+
+  const files = await readdir(folderPath);
+
+  if (files) {
+    console.log(files);
+  }
 };
 
 await list();
