5ebc61595279eddc76f4db45fd9c4ea4266fa787
Bella Makhova <rgvder@gmail.com>
2025-04-28 11:58:50 +0300

    feat: implement zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+import { formatPath } from '../utils/formatPath.js';
+
+const sourcePath = formatPath('zip', 'files', 'fileToCompress.txt');
+const destinationPath = formatPath('zip', 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(destinationPath);
+
+  await pipeline(source, gzip, destination);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createGunzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+import { formatPath } from '../utils/formatPath.js';
+
+const sourcePath = formatPath('zip', 'files', 'archive.gz');
+const destinationPath = formatPath('zip', 'files', 'fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = createGunzip();
+  const source = createReadStream(sourcePath);
+  const destination = createWriteStream(destinationPath);
+
+  await pipeline(source, gunzip, destination);
 };
 
 await decompress();
