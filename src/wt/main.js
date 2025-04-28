42177b2237056c4030add085c15f9143f42722e7
Bella Makhova <rgvder@gmail.com>
2025-04-28 12:03:42 +0300

    feat: implement wt functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,23 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'node:os';
+
+import { formatPath } from '../utils/formatPath.js';
+
+const filePath = formatPath('wt', 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
-};
+  const countNthFibonacci = (workerData) => {
+    return new Promise((resolve) => {
+      const worker = new Worker(filePath, { workerData });
+
+      worker.on('message', (data) => resolve({ status: 'resolved', data }));
+      worker.on('error', () => resolve({ status: 'error', data: null }));
+    });
+  };
+
+  const promises = cpus().map((_, index) => countNthFibonacci(index + 10));
+
+  console.log(await Promise.all(promises));
+}
 
 await performCalculations();
